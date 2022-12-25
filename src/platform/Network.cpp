6d60ec48aa340866cd21878b8544dc29ba3d8bf2
SuperBlockchain Pool <55582476+SuperBlockchain-Pool@users.noreply.github.com>
2022-12-25 17:15:37 +0100

    Update Network.cpp

--- src/platform/Network.cpp
+++ src/platform/Network.cpp
@@ -320,7 +320,8 @@ static_assert(BOOST_VERSION / 100000 == 1 && ((BOOST_VERSION / 100) % 1000) >= 6
 
 #ifdef _WIN32
 #include <Wincrypt.h>
-   // OpenSSL library
+#pragma comment(lib, "libcrypto.lib")  // OpenSSL library
+#pragma comment(lib, "libssl.lib")     // OpenSSL library
 #pragma comment(lib, "crypt32.lib")    // Windows SDK dependency of OpenSSL
 
 static void add_system_root_certs(ssl::context &ctx) {
